312c808af7808bcde0a06bd19b16dedb23c14c8d
ArturPalczynski <artur.palczynski@gmail.com>
2015-10-14 19:36:00 +0200

    First commit

--- KruskalAlgorithm/BubleSorting.java
+++ KruskalAlgorithm/BubleSorting.java
@@ -2,33 +2,31 @@ package KruskalAlgorithm;
 
 import java.util.ArrayList;
 
-
 public class BubleSorting {
-	
-	public static ArrayList<Edge> sortEdges(ArrayList<Edge> edgeList){
-		
+
+	public static ArrayList<Edge> sortEdges(ArrayList<Edge> edgeList) {
+
 		int size = edgeList.size();
 		ArrayList<Edge> outputList = new ArrayList<Edge>();
-		
-		for(int i=0;i<size;i++){
-			
-			for(int j=0;j<size-1;j++){
-				
-				if(edgeList.get(j).getWeight() > edgeList.get(j+1).getWeight()){
-					
+
+		for (int i = 0; i < size; i++) {
+
+			for (int j = 0; j < size - 1; j++) {
+
+				if (edgeList.get(j).getWeight() > edgeList.get(j + 1)
+						.getWeight()) {
+
 					Edge tempEdge = edgeList.get(j);
-					edgeList.set(j, edgeList.get(j+1));
-					edgeList.set(j+1, tempEdge);
-					
+					edgeList.set(j, edgeList.get(j + 1));
+					edgeList.set(j + 1, tempEdge);
+
 				}
-				
+
 			}
-			
+
 		}
 		return outputList;
-		
+
 	}
-	
-		
-	
+
 }
--- KruskalAlgorithm/Kruskal.java
+++ KruskalAlgorithm/Kruskal.java
@@ -16,8 +16,8 @@ public class Kruskal {
 		Edge edgeEight = new Edge(4, new char[] { 'c', 'f' }, 8);
 		Edge edgeNine = new Edge(2, new char[] { 'd', 'f' }, 9);
 		Edge edgeTen = new Edge(6, new char[] { 'e', 'f' }, 10);
-		//Edge edgeEleven = new Edge(35, new char[] { 'g', 'h' });
-		//Edge edgeTwelve = new Edge(44, new char[] { 'e', 'h' });
+		// Edge edgeEleven = new Edge(35, new char[] { 'g', 'h' });
+		// Edge edgeTwelve = new Edge(44, new char[] { 'e', 'h' });
 
 		ArrayList<Edge> inputEdges = new ArrayList<Edge>();
 
@@ -31,49 +31,46 @@ public class Kruskal {
 		inputEdges.add(edgeFive);
 		inputEdges.add(edgeSix);
 		inputEdges.add(edgeTen);
-		//kruskalTree.add(edgeEleven);
-		//kruskalTree.add(edgeTwelve);
+		// kruskalTree.add(edgeEleven);
+		// kruskalTree.add(edgeTwelve);
 
-		//sortowanie listy krawêdzi po wzglêdem wagi krawêdzi (niemalej¹co)
+		// sortowanie listy krawêdzi po wzglêdem wagi krawêdzi (niemalej¹co)
 		BubleSorting.sortEdges(inputEdges);
 
-		
 		ArrayList<Edge> minimalSpanningTree = new ArrayList<Edge>();
-		
-		
+
 		minimalSpanningTree.add(inputEdges.get(0));
-		minimalSpanningTree.get(0).setMembershipFlag(1);  
-		
-		
-		
+		minimalSpanningTree.get(0).setMembershipFlag(1);
+
 		for (int i = 0; i < inputEdges.size(); i++) {
-			
-			
-			if(minimalSpanningTree.get(i).getMembershipFlag() == inputEdges.get(i).getMembershipFlag()){
-				
+
+			if (minimalSpanningTree.get(i).getMembershipFlag() == inputEdges
+					.get(i).getMembershipFlag()) {
+
 				System.out.print("Loop ");
-				
-			}else{
-				
-			
+
+			} else {
+
 			}
 
 		}
 
 		System.out.println();
 		int weightSum = 0;
-		
+
 		for (int t = 0; t < minimalSpanningTree.size(); t++) {
-			
+
 			System.out.println(minimalSpanningTree.get(t).getTip());
 			weightSum += minimalSpanningTree.get(t).getWeight();
 		}
 
 		System.out.println(weightSum);
-		
-		//TODO
-		//Napisaæ funkcjê która sprawdza czy wierzcho³ki nowo dodawanej krawêdzi maj¹ obydwie falgi z tego samego drzewa
-		//jeœli tak to j¹ od¿ucamy a jeœli nie to dodajemy je do minimalnego drzewa i zmieniamy falgi na takie 
-		//jakie by³y w wiêkszym dzrewie
+
+		// TODO
+		// Napisaæ funkcjê która sprawdza czy wierzcho³ki nowo dodawanej
+		// krawêdzi maj¹ obydwie falgi z tego samego drzewa
+		// jeœli tak to j¹ od¿ucamy a jeœli nie to dodajemy je do minimalnego
+		// drzewa i zmieniamy falgi na takie
+		// jakie by³y w wiêkszym dzrewie
 	}
 }
